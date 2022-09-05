b41fb55d9b39362368319431e40de6747030175c
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-05 13:06:07 +0530

    initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main(){
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    //create a list
+    val immutableList1 = listOf("Guru Prasad","Vishnu","Devaki")
+    val immutableList2 = listOf(0,2,3,9,0,8,6,4,2)
+
+    for(item in immutableList1){
+        println(item)
+    }
+    println("")
+
+    //List access using index integer
+
+    println(immutableList1[1])
+
+    println("")
+
+
+    println("List : $immutableList2")
+
+    println("")
+    //sorted List
+
+    println("Sorted : ${immutableList2.sorted()}")
+
 }
